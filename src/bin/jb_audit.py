7a141b496d4bc653a332a92ffdf6a618904eb70b
JohnWall2016 <5672618@qq.com>
2020-03-11 14:56:42 +0800

    update

--- src/bin/jb_audit.py
+++ src/bin/jb_audit.py
@@ -22,6 +22,12 @@ def audit(start_date:str='', end_date:str=''):
 
     print(f'{start_date=}, {end_date=}')
 
+    time_span = ''
+    if start_date:
+        time_span += start_date
+    if end_date:
+        time_span += '_' + end_date
+
     result = None
 
     with Session.use() as session:
@@ -31,15 +37,40 @@ def audit(start_date:str='', end_date:str=''):
     if result is not None:
         with db.Session() as session:
             if len(result.datas):
+                print(f'共计 {len(result.datas)} 条')
                 index = 1
+                findex = 2
+                sindex = 2
+                export = False
+                dir = r'D:\精准扶贫'
+                workbook = Workbook(f'{dir}\\批量信息变更模板.xls')
+                sheet = workbook.get_sheet(0)
                 for cbsh in result.datas:
                     r = session.query(db.FPHistoryData).filter_by(idcard=cbsh.idcard).first()
                     if r:
                         print(f'{index:3}. {cbsh.idcard} {cbsh.name} {cbsh.birthday} ' + 
                               f'{r.jbrdsf} {r.name if r.name != cbsh.name else ""}')
+                        row = sheet.copy_row(findex, sindex)
+                        row.update('B', cbsh.idcard)
+                        row.update('E', cbsh.name)
+                        row.update('J', jb_class[r.jbrdsf])
+                        sindex += 1
+                        export = True
                     else:
                         print(f'{index:3}. {cbsh.idcard} {cbsh.name} {cbsh.birthday}')
                     index += 1
+                if export:
+                    workbook.save(f'{dir}\\批量信息变更{time_span}.xls')
+
+
+jb_class = {
+    '贫困人口一级': '051',
+    '特困一级': '031',
+    '低保对象一级': '061',
+    '低保对象二级': '062',
+    '残一级': '021',
+    '残二级': '022'
+}
 
 
 if __name__ == '__main__':
