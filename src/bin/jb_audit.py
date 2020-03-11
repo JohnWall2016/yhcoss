ce198be310551453bdab4c1d6a9ab66b2801bb21
JohnWall2016 <5672618@qq.com>
2020-03-11 15:21:43 +0800

    update

--- src/bin/jb_audit.py
+++ src/bin/jb_audit.py
@@ -34,33 +34,32 @@ def audit(start_date:str='', end_date:str=''):
         session.request_service(CbshQuery.new(start_date, end_date or ''))
         result = session.get_result(Cbsh)
 
-    if result is not None:
+    if result is not None and len(result.datas) > 0:
         with db.Session() as session:
-            if len(result.datas):
-                print(f'共计 {len(result.datas)} 条')
-                index = 1
-                findex = 2
-                sindex = 2
-                export = False
-                dir = r'D:\精准扶贫'
-                workbook = Workbook(f'{dir}\\批量信息变更模板.xls')
-                sheet = workbook.get_sheet(0)
-                for cbsh in result.datas:
-                    r = session.query(db.FPHistoryData).filter_by(idcard=cbsh.idcard).first()
-                    if r:
-                        print(f'{index:3}. {cbsh.idcard} {cbsh.name} {cbsh.birthday} ' + 
-                              f'{r.jbrdsf} {r.name if r.name != cbsh.name else ""}')
-                        row = sheet.copy_row(findex, sindex)
-                        row.update('B', cbsh.idcard)
-                        row.update('E', cbsh.name)
-                        row.update('J', jb_class[r.jbrdsf])
-                        sindex += 1
-                        export = True
-                    else:
-                        print(f'{index:3}. {cbsh.idcard} {cbsh.name} {cbsh.birthday}')
-                    index += 1
-                if export:
-                    workbook.save(f'{dir}\\批量信息变更{time_span}.xls')
+            print(f'共计 {len(result.datas)} 条')
+            index = 1
+            findex = 2
+            sindex = 2
+            export = False
+            dir = r'D:\精准扶贫'
+            workbook = Workbook(f'{dir}\\批量信息变更模板.xls')
+            sheet = workbook.get_sheet(0)
+            for cbsh in result.datas:
+                r = session.query(db.FPHistoryData).filter_by(idcard=cbsh.idcard).first()
+                if r:
+                    print(f'{index:3}. {cbsh.idcard} {cbsh.name} {cbsh.birthday} ' + 
+                          f'{r.jbrdsf} {r.name if r.name != cbsh.name else ""}')
+                    row = sheet.copy_row(findex, sindex)
+                    row.update('B', cbsh.idcard)
+                    row.update('E', cbsh.name)
+                    row.update('J', jb_class[r.jbrdsf])
+                    sindex += 1
+                    export = True
+                else:
+                    print(f'{index:3}. {cbsh.idcard} {cbsh.name} {cbsh.birthday}')
+                index += 1
+            if export:
+                workbook.save(f'{dir}\\批量信息变更{time_span}.xls')
 
 
 jb_class = {
